77f76f8dfc542b719182b9b814743fc58523fa72
Daniel Lanfear <lanfeadb@udmercy.edu>
2023-01-06 10:42:58 -0500

    cleaned up code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# using flask_restful
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, fields, marshal_with
 from firebase_admin import credentials, firestore, initialize_app
@@ -9,6 +8,7 @@ app = Flask(__name__)
 # creating an API object
 api = Api(app)
 
+# Initializing Firestore Database
 cred = credentials.Certificate('db_key.json')
 default_app = initialize_app(cred)
 db = firestore.client()
@@ -35,11 +35,7 @@ class Tests(Resource):
                 return all_tests, 200
         except Exception as e:
             return f"An Error Occured: {e}"
-        # return tests
 
-
-class Test(Resource):
-    @marshal_with(resource_fields)
     def post(self):
         try:
             id = request.json['id']
@@ -60,43 +56,7 @@ class Test(Resource):
             return f"An Error Occured: {e}"
 
 
-# making a class for a particular resource
-# the get, post methods correspond to get and post requests
-# they are automatically mapped by flask_restful.
-# other methods include put, delete, etc.
-class Hello(Resource):
-
-    # corresponds to the GET request.
-    # this function is called whenever there
-    # is a GET request for this resource
-    def get(self):
-
-        return jsonify({'message': 'hello world'})
-
-    # Corresponds to POST request
-    def post(self):
-
-        data = request.get_json()     # status code
-        return jsonify({'data': data}), 201
-
-  # another resource to calculate the square of a number
-
-
-# class Square(Resource):
-
-#     def get(self, num):
-
-#         return jsonify({'square': num**2})
-
-
-# adding the defined resources along with their corresponding urls
-api.add_resource(Hello, '/')
-# api.add_resource(Square, '/square/<int:num>')
 api.add_resource(Tests, '/tests')
-api.add_resource(Test, '/test')
 
-
-# driver function
 if __name__ == '__main__':
-
     app.run(debug=True)
